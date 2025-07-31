99b663c1fea999ad52b99e453c5f09b64c81fe58
Cypher-glitch1 <benjpwilson06@gmail.com>
2025-07-31 13:39:33 -0400

    part 1
    got it to work

--- lab4/part1/main.js
+++ lab4/part1/main.js
@@ -11,7 +11,7 @@ function randomValueFromArray(array){
 
 //2. RAW TEXT STRINGS
 
-const storytext = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const storytext = 'It was 94 fahrenheit outside, so insertx went for a walk. When they got to inserty, they stared in horror for a few moments, then insertz. Bob saw the whole thing, but was not surprised — insertx weighs 300 pounds, and it was a hot day.';
 
 const charactorX = ['Willy the Goblin','Big Daddy','Father Christmas'];
 
@@ -49,6 +49,6 @@ function result() {
 
   }
 
-  story.textContent = ;
+  story.textContent = newstory;
   story.style.visibility = 'visible';
 }
