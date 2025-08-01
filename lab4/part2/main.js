79f284590038635c035b112e737907631c3fa44e
Cypher-glitch1 <benjpwilson06@gmail.com>
2025-07-31 21:57:20 -0400

    Update main.js
    adding the image text

--- lab4/part2/main.js
+++ lab4/part2/main.js
@@ -5,10 +5,14 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
-const images = ["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"]
+const images = ["pic1.jpg","pic2.jpg","pic3.jpg","pic4.jpg","pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
-
+const alts = {"pic1.jpg":"a zoomed in pic of an eye",
+    "pic2.jpg":"a wave that looks like a cloud",
+    "pic3.jpg":"purple flowers",
+    "pic4.jpg":"egypt painting",
+    "pic5.jpg":"a big butter fly"}
 /* Looping through images */
 
 const newImage = document.createElement('img');
